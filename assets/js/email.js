755bc3cd111ec9f462641997e2d642fabec45598
Shenzlule <shenzlule65@gmail.com>
2024-01-03 13:10:01 +0300

    editted some features

---
+++ assets/js/email.js
@@ -0,0 +1,26 @@
+
+	<script src="https://smtpjs.com/v3/smtp.js">
+	</script>
+
+	<script type="text/javascript">
+		function sendEmail() {
+			Email.send({
+				Host: "smtp.gmail.com",
+				Username: "sender@email_address.com",
+				Password: "Enter your password",
+				To: 'receiver@email_address.com',
+				From: "sender@email_address.com",
+				Subject: "Sending Email using javascript",
+				Body: "Well that was easy!!",
+			})
+				.then(function (message) {
+					alert("mail sent successfully")
+				});
+		}
+	</script>
+
+	<form method="post">
+		<input type="button" value="Send Email"
+			onclick="sendEmail()" />
+	</form>
+
