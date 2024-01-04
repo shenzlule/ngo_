21e273f0a918b128a9060bccfa3c82eef409e5f7
Shenzlule <shenzlule65@gmail.com>
2024-01-04 19:13:55 +0300

    final commit of the day

---
+++ assets/jsonn/jsonn.js
@@ -0,0 +1,12 @@
+const links=[
+  {"facebook": "www.facebook.com"},
+  {"twitter" : "www.X.com"},
+]
+
+
+console.log(links.filter(link => link.facebook))
+
+
+const {facebook} = links[0]
+
+console.log(facebook)
